32e0133689f3224f77cc23684ab43c0b98c6baae
WTY <605770736@qq.com>
2018-12-13 16:52:07 +0800

    Initial Commit

---
+++ pages/first/first.js
@@ -0,0 +1,94 @@
+// pages/first.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navItems: [
+      {
+        name: '积分',
+        url: 'score'
+      },
+      {
+        name: '酬金',
+        url: 'wages',
+        isSplot: true
+      },
+      {
+        name: '积分商城',
+        url: 'out'
+      },
+      {
+        name: '活动',
+        url: 'bill'
+      },
+      {
+        name: '交流中心',
+        url: 'bill',
+        isSplot: true
+      },
+      {
+        name: '消息',
+        url: 'bill'
+      }
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
